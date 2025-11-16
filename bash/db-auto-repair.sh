dec6525823ecc64e98e02c8266ac116d3d8d3b0d
Oleksii Kuznietsov <utilmind@gmail.com>
2025-11-16 18:14:10 -0500

    Create db-auto-repair.sh

---
+++ bash/db-auto-repair.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+. "$(dirname "$BASH_SOURCE")/.credentials.sh"
+#
+# (c) https://github.com/utilmind
+#
+# This script good for crontab, set on @reboot:
+#     @reboot /var/www/_db_utils/mysql_auto_repair.sh
+#
+# However it's good only for local database only. Not needed for RDS instances of AWS, they are maintained separately.
+
+# check & auto-repair mySQL tables
+mysqlcheck --check --auto-repair --verbose --host=$dbHost --port=$dbPort --databases $dbName --user=$dbUsername --password=$dbPassword
